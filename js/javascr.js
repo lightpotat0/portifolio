7c0ed68fd6964f5a4d52651ce908ea5736d40c89
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2025-03-15 23:04:16 -0300

    updates

--- js/javascr.js
+++ js/javascr.js
@@ -1,6 +1,9 @@
 let addButton = document.getElementById("add_ab");
 let abas = document.querySelectorAll(".abs");
 let abas1 = document.querySelectorAll(".abs1");
+let box1 = document.querySelectorAll("#box1");
+let box_text = document.querySelectorAll(".box_skills");
+let curriculo = document.querySelectorAll("#curriculo");
 
 let currentTab = 0;
 
@@ -9,10 +12,23 @@ addButton.addEventListener("click", function () {
         abas.forEach(function (abs) {
             abs.style.display = "block";
         });
+        box_text.forEach(function (box) {
+            box.style.display = "block";
+        });
+        box1.forEach(function (box1) {
+            box1.style.display = "none";
+        })
+
     } else {
         abas1.forEach(function (abs1) {
             abs1.style.display = "block";
         });
+        box_text.forEach(function (box) {
+            box.style.display = "none";
+        });
+        curriculo.forEach(function (box3) {
+            box3.style.display = "block";
+        })
     }
 
     currentTab++;
@@ -25,7 +41,7 @@ let popup = document.getElementById("pr_popup")
 projects.forEach((project) => {
     project.addEventListener("click", function () {
         popup.style.display = "flex";
-    }); 
+    });
 });
 
 close.addEventListener("click", function () {
